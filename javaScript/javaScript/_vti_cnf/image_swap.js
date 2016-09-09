6b15e2b21cc822fda606ef238cfe24cd84d472cf
Niphaphan Nernklang <nipan.att00@gmail.com>
2016-09-09 17:58:54 -0500

    add js

---
+++ javaScript/javaScript/_vti_cnf/image_swap.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|17 Nov 2011 18:16:36 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|Annie_hoolay\\Annie
+vti_modifiedby:SR|Annie_hoolay\\Annie
+vti_timecreated:TR|17 Nov 2011 18:16:36 -0000
+vti_cacheddtm:TX|17 Nov 2011 18:16:36 -0000
+vti_filesize:IR|116
+vti_backlinkinfo:VX|gallery.html
---
+++ javaScript/javaScript/_vti_cnf/image_swap_library.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|12 May 2009 17:37:16 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|Annie_hoolay\\Annie
+vti_modifiedby:SR|Annie_hoolay\\Annie
+vti_timecreated:TR|12 May 2009 17:37:16 -0000
+vti_cacheddtm:TX|12 May 2009 17:37:16 -0000
+vti_filesize:IR|2067
+vti_backlinkinfo:VX|gallery.html
